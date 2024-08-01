41db57b60a53a480853f23d39c08a8acc0c6510f
Petr Emelianov <peter@Petrs-Laptop.local>
2024-08-01 11:14:41 +0300

    Initial

---
+++ src/config.py
@@ -0,0 +1,6 @@
+RANDOM_SEED = 42
+TRAIN_DATA_CSV_PATH = 'data/train_data.csv'
+TEST_DATA_CSV_PATH = 'data/test_data.csv'
+TRAIN_DATA_NPY_PATH = 'data/scaled_train_data.npy'
+TEST_DATA_NPY_PATH = 'data/scaled_test_data.npy'
+MODEL_PKL_PATH = 'data/model.pkl'
